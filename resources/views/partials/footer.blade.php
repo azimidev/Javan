99e85969bc69d060a926e1b68f66ba0ebb0326e4
Amir Azimi <hazz.azimi@gmail.com>
2016-07-29 18:14:50 +0100

    Improves Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -39,14 +39,15 @@
 				<a href="https://twitter.com/JavanLondon" class="text-info" target="_blank" title="Twitter">
 					<i class="fa fa-twitter-square fa-fw fa-3x"></i>
 				</a>
-				<a href="https://plus.google.com/b/107724180985175918891/" class="text-danger" target="_blank" title="Google Plus">
+				<a href="https://plus.google.com/b/107724180985175918891/" class="text-danger" target="_blank"
+				   title="Google Plus">
 					<i class="fa fa-google-plus-square fa-fw fa-3x"></i>
 				</a>
 				<a href="https://www.tripadvisor.co.uk/Restaurant_Review-g186338-d10515099-Reviews-Javan_Restaurant-London_England.html-m24177"
 				   class="text-success" target="_blank" title="Trip Advisor">
 					<span class="fa-stack fa-lg" style="height: 3.5em; width: 2.7em;">
 					  <i class="fa fa-square fa-stack-2x" style="font-size: 2.3em;"></i>
-					  <i class="fa fa-tripadvisor fa-stack-1x" style="color: #000;font-size: 1.3em;	line-height: 1.7em;"></i>
+					  <i class="fa fa-tripadvisor fa-stack-1x" style="color: #000; font-size: 1.3em;	line-height: 1.7em;"></i>
 					</span>
 				</a>
 			</div>

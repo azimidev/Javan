e5fe8ba4898f618a3f28272bf09720f138ada991
Amir Azimi <hazz.azimi@gmail.com>
2016-08-05 18:55:38 +0100

    Fix bug in blog for empty posts

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -14,33 +14,40 @@
 		</div>
 	</div>
 	<main class="container main main-raised" id="pjax-container">
-		<article>
-			<div class="col-md-8 col-xs-12">
-				@if ($post)
-					@section('title', $post->subject)
+		@if( ! $posts->isEmpty())
+			<article>
+				<div class="col-md-8 col-xs-12">
+					@if ($post)
+						@section('title', $post->subject)
 					@include('partials.post-show', ['post' => $post])
 					@include('partials.disqus')
-				@else
-					@include('partials.post-show', ['post' => $posts->first()])
-				@endif
-			</div>
-		</article>
-		<aside>
-			<div class="col-md-4 col-xs-12">
-				<h2><i class="fa fa-paper-plane-o"></i> Recent Posts</h2>
-				<div class="list-group">
-					@foreach ($posts as $post)
-						<a class="list-group-item {{ route_parameter('slug', $post->slug) ? 'active' : '' }}"
-						   id="pjax" href="{{ route('blog', $post->slug) }}">
-							{!! status($post) !!} {{ $post->subject }}
-						</a>
-					@endforeach
+					@else
+						@include('partials.post-show', ['post' => $posts->first()])
+					@endif
 				</div>
-				<div class="center">
-					{{ $posts->render() }}
-					{{-- $posts->appends(request()->input())->links() --}}
+			</article>
+			<aside>
+				<div class="col-md-4 col-xs-12">
+					<h2><i class="fa fa-paper-plane-o"></i> Recent Posts</h2>
+					<div class="list-group">
+						@foreach ($posts as $post)
+							<a class="list-group-item {{ route_parameter('slug', $post->slug) ? 'active' : '' }}"
+							   id="pjax" href="{{ route('blog', $post->slug) }}">
+								{!! status($post) !!} {{ $post->subject }}
+							</a>
+						@endforeach
+					</div>
+					<div class="center">
+						{{ $posts->render() }}
+						{{-- $posts->appends(request()->input())->links() --}}
+					</div>
 				</div>
+			</aside>
+		@else
+			<div class="center">
+				<h1>There is no post at the moment</h1>
+				<h2>Check back later</h2>
 			</div>
-		</aside>
+		@endif
 	</main>
 @stop

f4121d36fe826d05f3786cc80ca4ad80946217ca
Amir Azimi <hazz.azimi@gmail.com>
2016-08-19 22:50:04 +0100

    Change Pjax

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,7 +3,7 @@
 # Pages
 Route::get('/', 'PagesController@home');
 Route::get('about', 'PagesController@about');
-Route::get('menu', 'PagesController@menu');
+Route::get('/menu', 'PagesController@menu');
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 Route::get('information', 'PagesController@information');

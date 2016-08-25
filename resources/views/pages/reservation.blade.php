f70dede02ea7ea21be7e2d7f9234bc31cef3dfbd
Amir Azimi <hazz.azimi@gmail.com>
2016-08-25 19:17:01 +0100

    Bring Yelp reservation up

--- resources/views/pages/reservation.blade.php
+++ resources/views/pages/reservation.blade.php
@@ -18,99 +18,96 @@
 	<main class="main main-raised container">
 		<article>
 			<div class="col-sm-8">
+				<h2><i class="fa fa-yelp fa-fw"></i> Use Yelp Reservation</h2>
 				<div class="alert alert-inverse">
 					<p class="text-justify">
 						<i class="fa fa-info-circle fa-lg fa-fw"></i>
 						If you don't like to register simply use Yelp Reservation
 					</p>
 				</div>
-				<h2><i class="fa fa-yelp fa-fw"></i> Use Yelp Reservation</h2>
 				@include('partials.yelp-reservation')
 				<hr>
 				<form class="form-horizontal" action="{{ route('store.reservation') }}" method="POST" role="form">
-					<fieldset>
-						<legend>Please complete the form below</legend>
-						{{ csrf_field() }}
-						<div class="alert alert-inverse">
-							<p class="text-justify">
-								<i class="fa fa-info-circle fa-lg fa-fw"></i>
-								If you are a member please <a class="alert-link" href="{{ url('/login') }}">login here</a>
-								to book a table.
-							</p>
-						</div>
+					{{ csrf_field() }}
+					<div class="alert alert-inverse">
+						<p class="text-justify">
+							<i class="fa fa-info-circle fa-lg fa-fw"></i>
+							If you are a member please <a class="alert-link" href="{{ url('/login') }}">login here</a>
+							to book a table, otherwise use the form below to book tables
+						</p>
+					</div>
 
-						<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-							<label for="date" class="control-label col-sm-2">Name</label>
-							<div class="col-sm-5">
-								<input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}"
-								       placeholder="Full Name" minlength="3" maxlength="50" pattern="[a-zA-Z\s]+" required>
-								@if ($errors->has('name'))
-									<span class="help-block text-danger"><strong>{{ $errors->first('name') }}</strong></span>
-								@else
-									<span class="help-block text-info">Your full name here</span>
-								@endif
-							</div>
+					<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+						<label for="date" class="control-label col-sm-2">Name</label>
+						<div class="col-sm-5">
+							<input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}"
+							       placeholder="Full Name" minlength="3" maxlength="50" pattern="[a-zA-Z\s]+" required>
+							@if ($errors->has('name'))
+								<span class="help-block text-danger"><strong>{{ $errors->first('name') }}</strong></span>
+							@else
+								<span class="help-block text-info">Your full name here</span>
+							@endif
 						</div>
+					</div>
 
-						<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-							<label for="date" class="control-label col-sm-2">Email</label>
-							<div class="col-sm-5">
-								<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}"
-								       placeholder="Email" required>
-								@if ($errors->has('email'))
-									<span class="help-block text-danger"><strong>{{ $errors->first('email') }}</strong></span>
-								@else
-									<span class="help-block text-info">Your full email here</span>
-								@endif
-							</div>
+					<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+						<label for="date" class="control-label col-sm-2">Email</label>
+						<div class="col-sm-5">
+							<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}"
+							       placeholder="Email" required>
+							@if ($errors->has('email'))
+								<span class="help-block text-danger"><strong>{{ $errors->first('email') }}</strong></span>
+							@else
+								<span class="help-block text-info">Your full email here</span>
+							@endif
 						</div>
+					</div>
 
-						<div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
-							<label for="date" class="control-label col-sm-2">Phone</label>
-							<div class="col-sm-5">
-								<input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}"
-								       placeholder="Phone" required minlength="6" maxlength="20" pattern="^0[0-9\s]+">
-								@if ($errors->has('phone'))
-									<span class="help-block text-danger"><strong>{{ $errors->first('phone') }}</strong></span>
-								@else
-									<span class="help-block text-info">Your phone here in case we want to contact you</span>
-								@endif
-							</div>
+					<div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+						<label for="date" class="control-label col-sm-2">Phone</label>
+						<div class="col-sm-5">
+							<input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}"
+							       placeholder="Phone" required minlength="6" maxlength="20" pattern="^0[0-9\s]+">
+							@if ($errors->has('phone'))
+								<span class="help-block text-danger"><strong>{{ $errors->first('phone') }}</strong></span>
+							@else
+								<span class="help-block text-info">Your phone here in case we want to contact you</span>
+							@endif
 						</div>
+					</div>
 
-						<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-							<label for="date" class="control-label col-sm-2">Password</label>
-							<div class="col-sm-5">
-								<input id="password" type="password" class="form-control" name="password" value="{{ old('password') }}"
-								       required placeholder="Password" minlength="6" maxlength="30"
-								       pattern="(?=^.{6,}$)((?=.*\W+))(?![.\n]).*$">
-								@if ($errors->has('password'))
-									<span class="help-block text-danger"><strong>{{ $errors->first('password') }}</strong></span>
-								@else
-									<span class="help-block text-info">Your password here</span>
-								@endif
-							</div>
+					<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+						<label for="date" class="control-label col-sm-2">Password</label>
+						<div class="col-sm-5">
+							<input id="password" type="password" class="form-control" name="password" value="{{ old('password') }}"
+							       required placeholder="Password" minlength="6" maxlength="30"
+							       pattern="(?=^.{6,}$)((?=.*\W+))(?![.\n]).*$">
+							@if ($errors->has('password'))
+								<span class="help-block text-danger"><strong>{{ $errors->first('password') }}</strong></span>
+							@else
+								<span class="help-block text-info">Your password here</span>
+							@endif
 						</div>
+					</div>
 
-						<div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-							<label for="date" class="control-label col-sm-2">Confirm Password</label>
-							<div class="col-sm-5">
-								<input id="password_confirmation" type="password" class="form-control" name="password_confirmation"
-								       value="{{ old('password_confirmation') }}" placeholder="Password Confirmation" required
-								       minlength="6" maxlength="30" pattern="(?=^.{6,}$)((?=.*\W+))(?![.\n]).*$">
-								@if ($errors->has('password_confirmation'))
-									<span
-											class="help-block text-danger"><strong>{{ $errors->first('password_confirmation') }}</strong>
+					<div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+						<label for="date" class="control-label col-sm-2">Confirm Password</label>
+						<div class="col-sm-5">
+							<input id="password_confirmation" type="password" class="form-control" name="password_confirmation"
+							       value="{{ old('password_confirmation') }}" placeholder="Password Confirmation" required
+							       minlength="6" maxlength="30" pattern="(?=^.{6,}$)((?=.*\W+))(?![.\n]).*$">
+							@if ($errors->has('password_confirmation'))
+								<span
+										class="help-block text-danger"><strong>{{ $errors->first('password_confirmation') }}</strong>
 									</span>
-								@else
-									<span class="help-block text-info">Your password here again</span>
-								@endif
-							</div>
+							@else
+								<span class="help-block text-info">Your password here again</span>
+							@endif
 						</div>
+					</div>
 
-						@include('partials.reservations-create')
+					@include('partials.reservations-create')
 
-					</fieldset>
 				</form>
 			</div>
 		</article>

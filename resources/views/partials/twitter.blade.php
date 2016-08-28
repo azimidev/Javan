4e6de96b61999488e0c44193b84b36ef92f875c6
Amir Azimi <hazz.azimi@gmail.com>
2016-08-28 17:53:07 +0100

    Update Twitter widget

--- resources/views/partials/twitter.blade.php
+++ resources/views/partials/twitter.blade.php
@@ -1,4 +1,4 @@
-<a class="twitter-timeline" href="https://twitter.com/JavanLondon" data-link-color="#9C27B0"
+<a class="twitter-timeline" href="https://twitter.com/JavanLondon" data-link-color="#981CEB"
    data-chrome="nofooter noheader noscrollbar transparent" data-related="twitterapi,twitter"
    data-aria-polite="assertive" data-width="100%" lang="EN">Tweets by JavanLondon</a>
 <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>

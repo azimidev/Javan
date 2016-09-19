159321a12764808c79e815206b5883fc2bbcb2e9
Amir Azimi <hazz.azimi@gmail.com>
2016-09-19 15:34:28 +0100

    Change placeholder

--- resources/views/partials/deliverable.blade.php
+++ resources/views/partials/deliverable.blade.php
@@ -11,7 +11,7 @@
 					<div class="input-group input-group-lg">
 						<label class="sr-only" for="post_code"><i class="fa fa-envelope fa-fw fa-lg"></i></label>
 						<input type="text" class="form-control input-lg" name="post_code" id="post_code"
-						       placeholder="Post Code" required minlength="2" maxlength="8" pattern="^(^\S)[\w\s\-]+">
+						       placeholder="Enter Full Post Code" required minlength="2" maxlength="8" pattern="^(^\S)[\w\s\-]+">
 						<div class="input-group-btn">
 							<button type="submit" class="btn btn-raised btn-info">
 								<i class="fa fa-search fa-fw fa-lg"></i>

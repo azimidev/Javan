58a9ab1226e494984de1f152349b326042b27d2e
Amir Azimi <hazz.azimi@gmail.com>
2016-10-20 20:04:37 +0100

    Fix Stripe payment bug for cart

--- resources/views/partials/cart.blade.php
+++ resources/views/partials/cart.blade.php
@@ -84,4 +84,48 @@
 			});
 		});
 	</script>
+	<script src="https://js.stripe.com/v2/"></script>
+	<script>
+		/* <![CDATA[ */
+		(function() {
+			var StripeBilling = {
+
+				init : function() {
+					this.form              = $('#payment-form');
+					this.submitButton      = this.form.find('.submit');
+					this.submitButtonValue = this.submitButton.val();
+					Stripe.setPublishableKey('{{ config('services.stripe.key') }}');
+					this.bindEvents();
+				},
+
+				bindEvents : function() {
+					this.form.on('submit', $.proxy(this.sendToken, this));
+				},
+
+				sendToken : function(event) {
+					this.submitButton.val('One Moment').prop('disabled', true);
+					Stripe.createToken(this.form, $.proxy(this.stripeResponseHandler, this));
+					event.preventDefault();
+				},
+
+				stripeResponseHandler : function(status, response) {
+					if (response.error) {
+						this.form.find('.payment-errors').show().text(response.error.message);
+						return this.submitButton.prop('disabled', false).val(this.submitButtonValue);
+					}
+
+					$('<input>', {
+						type  : 'hidden',
+						name  : 'stripe-token',
+						value : response.id
+					}).appendTo(this.form);
+
+					this.form[0].submit();
+				}
+			};
+
+			StripeBilling.init();
+		})();
+		/* ]]> */
+	</script>
 @stop

fd05eda30b2aa419ce7d3244f0c8c731eff4a5a3
Amir Azimi <hazz.azimi@gmail.com>
2016-12-13 15:29:32 +0000

    Change the grid

--- resources/views/partials/post-show.blade.php
+++ resources/views/partials/post-show.blade.php
@@ -1,15 +1,16 @@
 @if ($post)
-	<div class="pull-right">
+	<div class="col-sm-4 center">
 		@foreach ($post->photos as $photo)
 			<a href="/{{ $photo->path }}" data-lity>
 				<img class="img-space img-responsive img-thumbnail img-raised"
-				     style="margin-left:1em;"
-				     src="/{{ $photo->thumbnail_path }}"
+				     src="/{{ $photo->path }}"
 				     alt="{{ $photo->name }}">
 			</a>
 			<br>
 		@endforeach
 	</div>
-	<h2>{{ $post->subject }}</h2>
-	{!! nl2br($post->body) !!}
+	<div class="col-sm-8">
+		<h2>{{ $post->subject }}</h2>
+		{!! nl2br($post->body) !!}
+	</div>
 @endif

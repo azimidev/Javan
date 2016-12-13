2f822b869296d6bf8ab764bfa578d7b62f0f27cd
Amir Azimi <hazz.azimi@gmail.com>
2016-12-13 15:08:34 +0000

    Change the grid

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -17,9 +17,9 @@
 
 	<main class="container main main-raised">
 		<article>
-			<div class="col-sm-8">
+			<div class="col-sm-9">
 				<article>
-					<h1 class="hidden-xs">Why you should try our cuisine ?</h1>
+					<h2 class="hidden-xs">Why you should try our cuisine ?</h2>
 					<h3 class="visible-xs">Why you should try our cuisine ?</h3>
 					<p>Javan Restaurant is a Persian cuisine restaurant in a great location in King Street, West London. We serve
 						top quality meals. This restaurant established with the aim of familiarising people within this area with
@@ -27,6 +27,40 @@
 					<p>We recently changed the management, name, quality of food our staff and especially our head chef. Our head
 						chef is one of the best chefs in Iranian communities with many years experience in working for top Persian
 						restaurants in London. We now do food delivery to your home.</p>
+					<h2 class="hidden-xs">Some of the popular food</h2>
+					<h3 class="visible-xs">Some of the popular food</h3>
+					<div class="row">
+						<div class="col-sm-6">
+							<a href="/images/restaurant/chenjeh.jpg" data-lity title="Chelo Chenjeh" data-toggle="tooltip">
+								<img src="/images/restaurant/chenjeh.jpg"
+								     class="img-space img-rounded img-raised img-responsive"
+								     alt="javan Restaurant">
+							</a>
+						</div><!-- /.col-sm-6 -->
+						<div class="col-sm-6">
+							<a href="/images/restaurant/momtaz.jpg" data-lity title="Momtaz" data-toggle="tooltip">
+								<img src="/images/restaurant/momtaz.jpg"
+								     class="img-space img-rounded img-raised img-responsive"
+								     alt="javan Restaurant">
+							</a>
+						</div><!-- /.col-sm-6 -->
+					</div><!-- /.row -->
+					<div class="row">
+						<div class="col-sm-6">
+							<a href="/images/restaurant/shishlik.jpg" data-lity title="Shishlik - Lamb Chops" data-toggle="tooltip">
+								<img src="/images/restaurant/shishlik.jpg"
+								     class="img-space img-rounded img-raised img-responsive"
+								     alt="javan Restaurant">
+							</a>
+						</div><!-- /.col-sm-6 -->
+						<div class="col-sm-6">
+							<a href="/images/restaurant/soltani.jpg" data-lity title="Soltani" data-toggle="tooltip">
+								<img src="/images/restaurant/soltani.jpg"
+								     class="img-space img-rounded img-raised img-responsive"
+								     alt="javan Restaurant">
+							</a>
+						</div><!-- /.col-sm-6 -->
+					</div><!-- /.row -->
 					<h2 class="hidden-xs">We have space for your events</h2>
 					<h4 class="visible-xs">We have space for your events</h4>
 					<p>We have a bar floor downstairs of restaurant where you can book for your events such as parties, birthday
@@ -36,14 +70,14 @@
 						Come visit us, call us, experience and enjoy the best Persian cuisine in West London.</p>
 					<p>There is the panorama of restaurants interior as well as exterior. You are free to make it full screen and
 						inteact with it. </p>
-					<iframe width="100%" height="600" frameborder="0" style="border:0" allowfullscreen
-					        src="https://www.google.com/maps/embed/v1/streetview?location=51.4933%2C-0.2397&key={{ env('GOOGLE_API_KEY') }}"></iframe>
+					{{--<iframe width="100%" height="600" frameborder="0" style="border:0" allowfullscreen
+					        src="https://www.google.com/maps/embed/v1/streetview?location=51.4933%2C-0.2397&key={{ env('GOOGLE_API_KEY') }}"></iframe>--}}
 
 				</article>
 			</div>
 		</article>
 		<aside>
-			<div class="col-sm-4">
+			<div class="col-sm-3">
 				<a href="/images/restaurant/javan-day-right.JPG" data-lity>
 					<img src="/images/restaurant/tn-javan-day-right.JPG"
 					     class="pull-right img-space img-rounded img-raised img-responsive"
--- resources/views/partials/post-show.blade.php
+++ resources/views/partials/post-show.blade.php
@@ -1,9 +1,9 @@
 @if ($post)
 	<h2>{{ $post->subject }}</h2>
-	<div class="col-sm-8">
+	<div class="col-sm-7">
 		{!! nl2br($post->body) !!}
 	</div>
-	<div class="col-sm-4 center">
+	<div class="col-sm-5 center">
 		@foreach ($post->photos as $photo)
 			<a href="/{{ $photo->path }}" data-lity>
 				<img class="img-space img-responsive img-thumbnail img-raised"

3a7589075828ff7006e50988ccbe2e509828874c
Amir Azimi <hazz.azimi@gmail.com>
2017-01-14 18:13:51 +0000

    Dry post form

--- resources/views/pages/menu.blade.php
+++ resources/views/pages/menu.blade.php
@@ -137,10 +137,10 @@
 													    data-toggle="tooltip"
 													    data-placement="top"
 													    itemprop="name">{{ $main_course->title }}</h3>
-													<h6 class="visible-xs" style="cursor:help;" title="{{ $main_course->description }}"
+													<h5 class="visible-xs" style="cursor:help;" title="{{ $main_course->description }}"
 													    data-toggle="tooltip"
 													    data-placement="top"
-													    itemprop="name">{{ $main_course->title }}</h6>
+													    itemprop="name">{{ $main_course->title }}</h5>
 													<span class="text-primary"
 													      itemprop="price">£ {{ number_format($main_course->price / 100 , 2) }}
 														&nbsp;&nbsp;&nbsp;</span>

fcbc10084eeec9654b95841eec5462ff456076b2
Amir Azimi <hazz.azimi@gmail.com>
2017-05-14 19:10:38 +0100

    Redirect home page to menu

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,7 +4,7 @@ Route::get('uber', function() { return redirect('//eats.uber.com/stores/5e3716e3
 
 # Pages
 // Route::get('/', 'PagesController@home');
-Route::get('/', function() { return redirect()->route('menu'); });
+// Route::get('/', function() { return redirect()->route('menu'); });
 
 Route::get('about', 'PagesController@about');
 Route::get('Persian-Food-Delivery-London', 'PagesController@menu')->name('menu');
